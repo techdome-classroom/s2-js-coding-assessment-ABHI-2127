2fbada5ac78599f74498aef45b54fb45671c1a18
Burra Abhishek <abhishek21.burra@gmail.com>
2024-11-11 16:13:03 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const matchingBrackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
     
+    for (const char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+            console.log("result");
+        } else {
+            if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
 };
-
 module.exports = { isValid };
-
-
